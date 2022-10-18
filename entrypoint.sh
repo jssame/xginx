9f5fb57b28ff3c26994895881fcf02db8f085a18
jssame <116044736+jssame@users.noreply.github.com>
2022-10-18 02:41:05 -0700

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#Xray版本
-VER="latest"
-Vless_Path="/s233"
-Vless_UUID="0c925f6b-e17e-4577-9f5f-37fe90fc0dc8"
-Vmess_Path="/s244"
-Vmess_UUID="0c925f6b-e17e-4577-9f5f-37fe90fc0dc8"
-Share_Path="/share233"
 ProxySite="mirror.umd.edu"
+Vless_UUID="0c925f6b-e17e-4577-9f5f-37fe90fc0dc8"
+Vless_Path="/s233"
+Vmess_UUID="0c925f6b-e17e-4577-9f5f-37fe90fc0dc8"
+Vmess_Path="/s244"
+Share_Path="/share233"
+VER="latest"
 
 if [ "$VER" = "latest" ]; then
   VER=`wget -qO- "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | sed -n -r -e 's/.*"tag_name".+?"([vV0-9\.]+?)".*/\1/p'`
