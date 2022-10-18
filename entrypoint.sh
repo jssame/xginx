739a4393a2096731b57f8df411f788242487c4d4
jssame <116044736+jssame@users.noreply.github.com>
2022-10-18 02:22:34 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
 
 #Xray版本
-if [[ -z "${VER}" ]]; then
-  VER="latest"
-fi
-echo ${VER}
-
-if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/s233"
-fi
-echo ${Vless_Path}
-
-if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
-fi
-echo ${Vless_UUID}
-
-if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="/s244"
-fi
-echo ${Vmess_Path}
-
-if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
-fi
-echo ${Vmess_UUID}
-
-if [[ -z "${Share_Path}" ]]; then
-  Share_Path="/share233"
-fi
-echo ${Share_Path}
+VER="latest"
+Vless_Path="/s233"
+Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+Vmess_Path="/s244"
+Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+Share_Path="/share233"
+ProxySite="mirror.umd.edu"
 
 if [ "$VER" = "latest" ]; then
   VER=`wget -qO- "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | sed -n -r -e 's/.*"tag_name".+?"([vV0-9\.]+?)".*/\1/p'`
